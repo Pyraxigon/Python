ad6aec063350abe9540971d88b21a2c26ba0d56e
Pyraxigon <108704470+Pyraxigon@users.noreply.github.com>
2022-08-23 04:09:18 +0200

    Add files via upload

--- Number finder/Time for Number Python.py
+++ Number finder/Time for Number Python.py
@@ -5,52 +5,58 @@ Values = []
 Time_Values = []
 # Select1 = int(input("How many times do you want it to do? "))
 # Select1 is for the amount completions
-Select1 = 1_000
-# Select2 is for amount of times I want
-Select2 = 1000
+Select1 = int(input("How many Significant figures do you want? Maximum is 9: "))
+Select2 = int(input("How many iterations do you want it to complete: "))
+
+SF = Select1
+Select1 = 10**Select1
 
 
 Total_Tries = 0
 # to just print out a message
 y = 0
+x = 1
 Time_Data = True
-# Counts from start of whole thing
-Total_start = time.time()
-while Time_Data:
-    Data = True
-    Start = time.time()
-    while Data:
+R_Number = True
+Data = True
+Start = time.time()
+while Data:
+    y = y + 1
+    if y > (Select1/5):
+        x = x + 1
+        y = 0
+        if x % 2 == 1:
+            print("Calculating...")
+        else:
+            print("...Calculating")
 
-        # y = y + 1
-        # if y > (Select1/10):
-            # print("Calculating")
-            # y = 0
+    # Just the randomizer, and selection of Value to chose
+    if Data:
+        Number = random.randint(1, 1_000_000)
 
-        # Just the randomizer, and selection of Value to chose
-        if Data:
-            Number = random.randint(1, 1_000_000_000)
+        # Exact number
+        Correct = Number
 
-            # Exact number
-            Correct = Number
+        # to use for top value
+        Top_Digits = len(str(Number))
 
-            # to use for top value
-            Top_Digits = len(str(Number))
+        # To know how high the
+        Digits = len(str(Number))
+        Digits = -int(Digits)
+        Digits = Digits + 1
 
-            # To know how high the
-            Digits = len(str(Number))
-            Digits = -int(Digits)
-            Digits = Digits + 1
+        # High End
+        Top_Value = (10 ** Top_Digits) - 1
 
-            # High End
-            Top_Value = (10 ** Top_Digits) - 1
-
-            Min = 1
-            Top_Value = Top_Value
-            Guess = random.randint(Min, Top_Value)
+        Min = 1
+        Top_Value = Top_Value
+        Guess = random.randint(Min, Top_Value)
+        R_Number = False
 
         x = 0
         # The Bot Guesser
         Active = True
+
         while Active:
             x = x + 1
             Guess = random.randint(Min, Top_Value)
@@ -92,9 +98,9 @@ while Time_Data:
             Time = round(Time, 10)
             Time_Values.append(Time)
             # print(f"It took {Time}")
-            Data = False
+            Active = False
 
-    if len(Time_Values) > Select2:
+    if len(Time_Values)+1 > Select2:
         Total_End = time.time()
         print("\n\n")
         Time_Values.sort()
@@ -102,11 +108,12 @@ while Time_Data:
         #     print(i)
         print(f"Fastest was {Time_Values[0]}")
         print(f"Longest was {Time_Values[-1]}")
-        print(len(Time_Values))
+
         print(f"Average time was {sum(Time_Values) / len(Time_Values)}")
-        print(f"The amount of decimals = {Select1}")
-        print(f"The amount of irriterations was {Select2}")
-        Total_Time = Total_End - Total_start
+        print(f"The amount of digits was {SF}")
+        print(f"The amount of iterations was {Select2}")
+        Total_Time = Total_End - Start
         Total_Time = round(Total_Time, 10)
-        print(f"It took {Total_Time}")
+        print(f"The total time it took was: {Total_Time}")
         Time_Data = False
+        Data = False
